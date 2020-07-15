a94420209678b9782cdd1c2c87048578219d7cd5
Jonatan <tuskerfieldmac@Jony-MacBook-Pro.local>
2020-07-15 05:48:50 -0500

    Updates

---
+++ precache-manifest.f9728cedeea15d9a008e4477a2ba1ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc0970ea5aefc556220541837121874",
+    "url": "/monsters_app/index.html"
+  },
+  {
+    "revision": "bcc7f0429b2bc941e361",
+    "url": "/monsters_app/static/css/main.8b2f6df1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters_app/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_app/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc7f0429b2bc941e361",
+    "url": "/monsters_app/static/js/main.9920682c.chunk.js"
+  },
+  {
+    "revision": "ce2b26b0a44c9c9afb58",
+    "url": "/monsters_app/static/js/runtime-main.1931750b.js"
+  }
+]);
